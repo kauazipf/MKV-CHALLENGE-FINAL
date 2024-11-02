0cda0ddd1f517bd21ba24b090f2de8a67290f8af
kauazipf <rm558957@fiap.com.br>
2024-11-01 22:30:12 -0300

    Criação do arquivos types com os props e realização da pagina de carros.
    Co-authored-by: Victor Egidio Lira <Vitin46@users.noreply.github.com>
    Co-authored-by: marcelooou <marcelooou@users.noreply.github.com>

---
+++ mkvchallenge/src/components/Carros/index.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import { CarrosProps } from "@/types/types";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { GrEdit as Editar } from "react-icons/gr";
+import { RiDeleteBin2Line as Excluir } from "react-icons/ri";
+
+export default function Carros() {
+
+    const [carros, setProdutos] = useState<CarrosProps[]>([]);
+    
+    const chamadaApi = async ()=>{
+        const response = await fetch("/api/base-produtos");
+        const data = await response.json();
+        
+        setProdutos(data);
+    }
+
+    useEffect(() => {
+        chamadaApi();
+    }, [])
+
+const handleDelete = async (id:number)=>{
+    try {
+        const response = await fetch(`/api/base-produtos/${id}`,{
+            method: 'DELETE',
+        });
+        if (response.ok) {
+            alert("Produto excluído com sucesso.");
+            chamadaApi();
+        }
+} catch (error) {
+    console.error("Falha ao remover o produto: ", error);
+}
+}
+
+    return (
+        <div>
+            <h2>Carros</h2>
+
+            <table className="tabelaCarros">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NOME</th>
+                        <th>PRECO</th>
+                        <th>IMAGEM</th>
+                        <th>EDITAR | EXCLUIR</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {carros.map((p) => (
+                        <tr key={p.id}>
+                            <td>{p.id}</td>
+                            <td>{p.nome}</td>
+                            <td>{p.marca}</td>
+                            <td>{p.cor}</td>
+                            <td>{p.ano}</td>
+                            <td>{p.imagem}</td>
+                    <td><Link href={`/Carros/${p.id}`}><Editar className="inline text-3xl"/></Link> | 
+                        <Link href="#" onClick={()=> handleDelete(p.id)}> <Excluir className="inline text-3xl"/></Link> </td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={5}>
+                            Quantidade de carros : {carros.length}
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+
+        </div>
+    )
+}
---
+++ mkvchallenge/src/types/types.ts
@@ -0,0 +1,15 @@
+
+export type CarrosProps = {
+    id: number;
+    nome: string;
+    marca: string;
+    cor: string;
+    ano: string;
+    imagem: string
+  };
+
+export type IntegrantesProps = {
+    nome: string;
+    rm: string;
+    github: string;
+};
